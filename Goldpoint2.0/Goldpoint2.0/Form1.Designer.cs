26e90d4083de63b95d15daff57bf41615fa63109
shisan9527 <1044157143@qq.com>
2019-12-08 12:07:07 +0800

    Final Commit

---
+++ Goldpoint2.0/Goldpoint2.0/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace Goldpoint1._0
+{
+    partial class GoldPoint
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GoldPoint));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 22F);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(373, 80);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "黄 金 点 游 戏";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 16F);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label2.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label2.Location = new System.Drawing.Point(12, 221);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 40);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "玩家:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("微软雅黑", 18F);
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.textBox1.Location = new System.Drawing.Point(170, 221);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(193, 40);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "0";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 14F);
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button1.Location = new System.Drawing.Point(170, 283);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 40);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "开  始";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Font = new System.Drawing.Font("微软雅黑", 14F);
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button2.Location = new System.Drawing.Point(285, 283);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(80, 40);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "下一位";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Black;
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 10F);
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label3.Location = new System.Drawing.Point(0, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(373, 115);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "游戏规则：10个同学,每人写一个0~100之间的有理数 (不包括0或100)，交给裁判，裁判算出所有数字的平均值，然后乘以0.618（所谓黄金分割常数），得到G值" +
+    "。提交的数字最靠近G（取绝对值）的同学得到2分，离G最远的同学得到－1分，其他同学得0分.";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(14, 321);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 40);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Black;
+            this.label5.Font = new System.Drawing.Font("微软雅黑", 14F);
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label5.Location = new System.Drawing.Point(14, 283);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(106, 94);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "G点：";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ControlText;
+            this.button3.Font = new System.Drawing.Font("微软雅黑", 14F);
+            this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button3.Location = new System.Drawing.Point(285, 337);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(80, 40);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "统  计";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button4.Font = new System.Drawing.Font("微软雅黑", 14F);
+            this.button4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button4.Location = new System.Drawing.Point(170, 337);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(80, 40);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "重  置";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.Black;
+            this.label6.Font = new System.Drawing.Font("微软雅黑", 9F);
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label6.Location = new System.Drawing.Point(0, 400);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(373, 54);
+            this.label6.TabIndex = 11;
+            // 
+            // GoldPoint
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(375, 454);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "GoldPoint";
+            this.Text = "GoldPoint";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Goldpoint2.0/Goldpoint2.0/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Goldpoint1._0
+{
+    public partial class GoldPoint : Form
+    {
+        public GoldPoint()
+        {
+            InitializeComponent();
+        }
+        private int Players = 0;
+        private double[] numbers = new double[10];
+        private double[] socer = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };//new double [10];
+        private double Sum = 0, min, temp, max;
+        private int i, j;
+        private int maxno, minno;
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e) //重置
+        {
+            Players = 0;
+            Sum = min = temp = max = 0;
+            maxno = minno = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                numbers[i] = 0;
+                socer[i] = 0;
+            }
+            textBox1.Text = null;
+            label2.Text = "玩家：";
+            label5.Text = "G点:";
+            label6.Text = " 玩家1 " + "玩家2 " + "玩家3 " + "玩家4 " + "玩家5 " + "玩家6 " + "玩家7 " + "玩家8 " + "玩家9 " + "玩家10 " + "\n" + "    " + Convert.ToString(socer[0]) + "       " + Convert.ToString(socer[1]) + "       " + Convert.ToString(socer[2]) + "       " + Convert.ToString(socer[3]) + "       " + Convert.ToString(socer[4]) + "       " + Convert.ToString(socer[5]) + "       " + Convert.ToString(socer[6]) + "       " + Convert.ToString(socer[7]) + "        " + Convert.ToString(socer[8]) + "       " + Convert.ToString(socer[9]) + "       ";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Players = 0;
+            Sum = min = temp = max = 0;
+            maxno = minno = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                numbers[i] = 0;
+            }
+            label2.Text = "玩家" + Convert.ToString(Players + 1) + ":";
+            textBox1.Enabled = true;
+            textBox1.Text = null;
+            label6.Text = " 玩家1 " + "玩家2 " + "玩家3 " + "玩家4 " + "玩家5 " + "玩家6 " + "玩家7 " + "玩家8 " + "玩家9 " + "玩家10 " + "\n" + "    " + Convert.ToString(socer[0]) + "       " + Convert.ToString(socer[1]) + "       " + Convert.ToString(socer[2]) + "       " + Convert.ToString(socer[3]) + "       " + Convert.ToString(socer[4]) + "       " + Convert.ToString(socer[5]) + "       " + Convert.ToString(socer[6]) + "       " + Convert.ToString(socer[7]) + "        " + Convert.ToString(socer[8]) + "       " + Convert.ToString(socer[9]) + "       ";
+        }//开始
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+                
+            if (Players < 10)  //人数不到10个
+            {
+                numbers[Players] = Convert.ToDouble(textBox1.Text);
+                Sum = Sum + numbers[Players];
+                //button1_Click(this, e);
+                Players++;
+                if (Players < 10)
+                {
+                    label2.Text = "玩家:" + Convert.ToString(Players + 1);
+                    textBox1.Text = null;
+                }
+            }
+        }//下一位
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            if (Players == 10)
+            {
+                button2_Click(this, e);
+                textBox1.Enabled = false;
+                label5.Text = "G点：" + "\n" + Convert.ToString(Sum * 0.0618);
+                min = max = Math.Abs(numbers[i] - Sum * 0.0618);
+                for (int i = 0; i < 10; i++)
+                {
+                    j = i;
+                    temp = Math.Abs(numbers[i] - Sum * 0.0618);
+                    if (temp < min)
+                    {
+                        min = temp;
+                        minno = i;
+                    }
+                    if (temp > max)
+                    {
+                        max = temp;
+                        maxno = i;
+                    }
+                    //max = Math.Abs(numbers[i] - Sum * 0.618);
+                }
+
+                socer[minno] = socer[minno] + 2;
+                socer[maxno] = socer[maxno] - 1;
+
+                label6.Text = " 玩家1 " + "玩家2 " + "玩家3 " + "玩家4 " + "玩家5 " + "玩家6 " + "玩家7 " + "玩家8 " + "玩家9 " + "玩家10 " + "\n" + "    " + Convert.ToString(socer[0]) + "       " + Convert.ToString(socer[1]) + "       " + Convert.ToString(socer[2]) + "       " + Convert.ToString(socer[3]) + "       " + Convert.ToString(socer[4]) + "       " + Convert.ToString(socer[5]) + "       " + Convert.ToString(socer[6]) + "       " + Convert.ToString(socer[7]) + "        " + Convert.ToString(socer[8]) + "       " + Convert.ToString(socer[9]) + "       ";
+            }//统计
+           
+        }
+    }
+}
